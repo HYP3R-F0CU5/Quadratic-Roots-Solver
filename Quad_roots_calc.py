cc7b844694843f0f93a8aebc85b5c56cab62896a
Shujan <gamesmagic2408@gmail.com>
2021-11-18 09:32:34 +0000

    fixed square root problem + made more efficient
    Read the title

--- Quad_roots_calc.py
+++ Quad_roots_calc.py
@@ -1,10 +1,10 @@
-import math
 a = int(input("Please enter value a (ax^2 + bc + c): "))
 b = int(input("Please enter value b (ax^2 + bc + c): "))
 c = int(input("Please enter value c (ax^2 + bc + c): "))
 discrim = ( b ** 2 + (-4*a*c) )
 if discrim < 0:
-    discrim = math.sqrt((discrim / -1))
+    discrim = (discrim / -1) ** 0.5
     print(str(-b / (2*a)) + " +/- " + (str(discrim / (2*a)) + "i"))
 else:
+    discrim = (discrim) ** 0.5
     print((-b + discrim) / (2*a), (-b - discrim) / (2*a))
