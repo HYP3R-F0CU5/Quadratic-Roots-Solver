4a89760f029cffd1937f4c31a8e0277ca7631f36
Shujan <gamesmagic2408@gmail.com>
2021-11-18 09:23:03 +0000

    Imaginary Numbers Included
    Update that allows the program to return imaginary numbers!

--- Quad_roots_calc.py
+++ Quad_roots_calc.py
@@ -4,8 +4,16 @@ a = int(input("Please enter value a (ax^2 + bc + c): "))
 b = int(input("Please enter value b (ax^2 + bc + c): "))
 c = int(input("Please enter value c (ax^2 + bc + c): "))
 
-discrim = math.sqrt(( b ** 2 + (-4*a*c) ))
-print(discrim)
-ans1 = (-b + discrim) / (2*a)
-ans2 = (-b - discrim) / (2*a)
-print(ans1, ans2)
+discrim = ( b ** 2 + (-4*a*c) )
+if discrim < 0:
+    discrim /= -1
+    discrim = math.sqrt(discrim)
+    real = -b / (2*a)
+    imaginary = str(discrim / (2*a)) + "i"
+    ans = str(real) + " +/- " + imaginary
+    print(ans)
+else:
+    print(discrim)
+    ans1 = (-b + discrim) / (2*a)
+    ans2 = (-b - discrim) / (2*a)
+    print(ans1, ans2)
