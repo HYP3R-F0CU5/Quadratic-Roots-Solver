bb118ac1478fac67d06a42b87c310c6394707009
Shujan <gamesmagic2408@gmail.com>
2021-11-21 13:13:41 +0000

    Update Quad_roots_calc.py

--- Quad_roots_calc.py
+++ Quad_roots_calc.py
@@ -1,6 +1,6 @@
-a = int(input("Please enter value a (ax^2 + bc + c): "))
-b = int(input("Please enter value b (ax^2 + bc + c): "))
-c = int(input("Please enter value c (ax^2 + bc + c): "))
+a = float(input("Please enter value a (ax^2 + bc + c): "))
+b = float(input("Please enter value b (ax^2 + bc + c): "))
+c = float(input("Please enter value c (ax^2 + bc + c): "))
 discrim = ( b ** 2 + (-4*a*c) )
 if discrim < 0:
     discrim = (discrim / -1) ** 0.5
